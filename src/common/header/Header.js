f5fc2b1f589da85fee73bdb836ed1d01c687e4e1
vaibhav <vaibhavkumarjha46@gmail.com>
2022-04-14 23:50:51 +0530

    fixing bugg of Logo

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -55,7 +55,7 @@ function Header() {
             <img
                 src="https://cdn.upgrad.com/uploads/production/286e1f11-1897-4d0c-ab0f-6b2bfc1ce642/logo.svg"
                 alt="Logo"
-                id="logo"
+               className="headerLogo"
             />
             <div className="buttonContainer">
                 {isDetailsPage ? (
